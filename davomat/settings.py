8553029dd21820873af0c5fc413fee87d7fac9c8
dilshod <dilshodbaxriddinov97@gmail.com>
2025-07-10 16:51:52 +0500

    req

--- davomat/settings.py
+++ davomat/settings.py
@@ -60,7 +60,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ORIGIN_ALLOW_ALL = True
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = [
-    "https://davomat.masofaviyaudit.uz/",
+    "https://davomat.masofaviyaudit.uz",
 ]
 TEMPLATES = [
     {
